7f836d4aa68e76a1fd6f1fc13c918bbe4cd2d497
leo <lch01234@gmail.com>
2024-07-29 13:52:12 +0900

    formatting

--- structures/quaternion.lua
+++ structures/quaternion.lua
@@ -9,29 +9,27 @@ local matrix = require("structures.matrix")
 local Quaternion = lang.newclass("Quaternion")
 
 function Quaternion.identity()
-	---@type Quaternion
-	local q = Quaternion.new()
-	q.r = 1 -- cos(0)
-	q.i = 0
-	q.j = 0
-	q.k = 0
+  ---@type Quaternion
+  local q = Quaternion.new()
+  q.r = 1 -- cos(0)
+  q.i = 0
+  q.j = 0
+  q.k = 0
 
-	return q
+  return q
 end
 
 --3x3
 ---@return Matrix
 function Quaternion:matrix()
-	local r, i, j, k = self.r, self.i, self.j, self.k
+  local r, i, j, k = self.r, self.i, self.j, self.k
 
   -- stylua: ignore
-  local mat = {
+  return matrix.new(3, 3, {
     1 - 2 * (j * j + k * k), 2 * (i * j - r * k), 2 * (i * k + r * j),
     2 * (i * j + r * k), 1 - 2 * (i * i + k * k), 2 * (j * k - r * i),
     2 * (i * k - r * j), 2 * (j * k + r * i), 1 - 2 * (i * i + j * j),
-  }
-
-	return matrix.new(3, 3, table.unpack(mat))
+  })
 end
 
 return Quaternion
