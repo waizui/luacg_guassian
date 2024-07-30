00203cb128ce869adc815b2a78376a9241573abe
leo <lch01234@gmail.com>
2024-07-30 12:07:12 +0900

    comment

--- gauspt/splat.lua
+++ gauspt/splat.lua
@@ -8,6 +8,7 @@ local vector = require("structures.vector")
 ---@field scale Vector
 local Splat = lang.newclass("Splat")
 
+-- splat also has the propeties of SH coefficients, opacity value, but they are omited in this demo
 function Splat:ctor()
 	self.position = vector.new(3, 0, 0, 0)
 	self.rotation = quaternion.identity()
